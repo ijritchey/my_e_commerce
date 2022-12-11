2a1c6090600b650bb489db103a557e6976febb8a
ijritchey <ijritchey@hotmail.com>
2022-12-10 16:05:19 -0800

    created item views, urls and html pages. Also login/logout

--- e_commerce_app/urls.py
+++ e_commerce_app/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('items/', views.items_index, name='items_index'),
+    path('items/create/', views.ItemCreate.as_view(), name='items_create'),
+    path('items/<int:pk>/update', views.ItemUpdate.as_view(), name='itmes_update'),
+    path('items/<int:pk>/delete', views.ItemDelete.as_view(), name='itmes_delete'),
+    path('user/<username>', views.profile, name='profile'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
+    path('accounts/login/', views.login_view, name='require_login')
 ]
